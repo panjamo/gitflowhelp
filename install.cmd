1977491a4277d97a4dd31a98320220f15ea0a4f9
josch <jochen.schieberlein@thinprint.com>
2019-01-28 14:33:37 +0100

    vorbereitung powershell

--- install.cmd
+++ install.cmd
@@ -1,1 +1,10 @@
 git config --global include.path "%~dp0\githelper\.gitconfigalias"
+@ECHO OFF
+REM soll später mal in powershell umgeschieben werden
+REM mklink %SystemRoot%\gg.cmd  "%~dp0\githelper\gg.cmd"
+REM mklink %SystemRoot%\gitflowinit.cmd  "%~dp0\gitflowhelper\gitflowinit.cmd"
+
+REM https://www.powershellgallery.com/packages/PSGitLab/
+REM powershell: Install-Module -Name PSGitLab
+REM https://ctd-sv01.thinprint.de/profile/personal_access_tokens
+REM w:\GIT\gitflowhelper\gitlab\UpdateGitLabDirs.ps1
