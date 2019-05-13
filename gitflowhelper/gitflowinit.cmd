fa1040f61cfe54744db6385adbe56c24b16170e0
josch <jochen.schieberlein@thinprint.com>
2019-05-13 13:38:17 +0200

    no pom files any more filterd

--- gitflowhelper/gitflowinit.cmd
+++ gitflowhelper/gitflowinit.cmd
@@ -16,13 +16,13 @@ IF NOT ".%stashResult%" == ".No local changes to save" (
 echo *.rc diff=RC >> .git\info\attributes
 echo *.rc2 diff=RC >> .git\info\attributes
 echo *.man diff=MAN >> .git\info\attributes
-echo *.nuspec diff=NUSPEC >> .git\info\attributes
+REM "echo *.nuspec diff=NUSPEC >> .git\info\attributes"
 echo pom*.xml diff=POM >> .git\info\attributes
 echo AssemblyInfo.cs diff=ASSEMBLYINFO >> .git\info\attributes
 echo AssemblyInfo.cpp diff=ASSEMBLYINFO >> .git\info\attributes
 
 git config diff.NUSPEC.textconv "sed 's/<\(version\|copyright\)>.*<\/\(version\|copyright\)>//i'"
-git config diff.POM.textconv "sed 's/<version>.*<\/version>//i'"
+REM git config diff.POM.textconv "sed 's/<version>.*<\/version>//i'"
 git config diff.MAN.textconv "sed 's/version\s*=\s*.[0123456789.]*.//i'"
 git config diff.RC.textconv "sed 's/^\s*\(FILEVERSION\|PRODUCTVERSION\|VALUE \"FileDescription\",\|VALUE \"CompanyName\",\|VALUE \"LegalCopyright\",\|VALUE \"ProductVersion\",\|VALUE \"FileVersion\",\).*//i'"
 git config diff.ASSEMBLYINFO.textconv "sed 's/\(.*assembly:\s*\(AssemblyCopyright\|AssemblyInformationalVersion\|AssemblyFileVersion\|AssemblyVersion\|AssemblyCompany\|AssemblyInformationalVersionAttribute\|AssemblyFileVersionAttribute\|AssemblyVersionAttribute\).*\)\|( internal test release[^)]*)//i'"
