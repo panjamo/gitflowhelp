13c80ee9f736c2ba725742573c754cff009cb844
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2023-11-17 15:47:57 +0100

    traverse to all repos

--- gitlab/UpdateGitLabDirs.ps1
+++ gitlab/UpdateGitLabDirs.ps1
@@ -8,23 +8,23 @@
 
 $WshShell = New-Object -comObject WScript.Shell
 
-
-Get-GitLabProject | ForEach-Object {
+Get-GitLabNamespace | % { Get-GitLabProject -GroupId $_.Id -ErrorAction SilentlyContinue} | ForEach-Object {
     $_
     $MinusName = $_.path_with_namespace -replace "/", "---"
     $cwd = (Get-Location).Path + "/"
 
-    if ((New-Item -ItemType directory ($cwd + $_.path_with_namespace) -ErrorAction SilentlyContinue) -or $true) {
-        $filePath = $cwd + ($_.path_with_namespace + "/_git clone " + $MinusName + ".cmd")
-        [System.IO.File]::WriteAllText($filePath, "gg.cmd -p" + $_.ssh_url_to_repo, [System.Text.Encoding]::GetEncoding('iso-8859-1'))
+    if ($_.path_with_namespace) {
+        if ((New-Item -ItemType directory ($cwd + $_.path_with_namespace) -ErrorAction SilentlyContinue) -or $true) {
+            $filePath = $cwd + ($_.path_with_namespace + "/_git clone " + $MinusName + ".cmd")
+            [System.IO.File]::WriteAllText($filePath, "gg.cmd -p" + $_.ssh_url_to_repo, [System.Text.Encoding]::GetEncoding('iso-8859-1'))
 
-        $Shortcut = $WshShell.CreateShortcut([IO.Path]::GetFullPath($cwd + $MinusName + ".lnk"))
-        $Shortcut.TargetPath = [IO.Path]::GetFullPath($cwd + $_.path_with_namespace)
-        #$Shortcut.Arguments = '%*'
-        $Shortcut.Save()
+            $Shortcut = $WshShell.CreateShortcut([IO.Path]::GetFullPath($cwd + $MinusName + ".lnk"))
+            $Shortcut.TargetPath = [IO.Path]::GetFullPath($cwd + $_.path_with_namespace)
+            #$Shortcut.Arguments = '%*'
+            $Shortcut.Save()
+        }
+
+        $filePath = $cwd + ($_.path_with_namespace + "/gitlab " + $MinusName + ".url")
+        [System.IO.File]::WriteAllText($filePath, ("[InternetShortcut]`r`nURL=" + $_.web_url), [System.Text.Encoding]::GetEncoding('iso-8859-1'))
     }
-
-    $filePath = $cwd + ($_.path_with_namespace + "/gitlab " + $MinusName + ".url")
-    [System.IO.File]::WriteAllText($filePath, ("[InternetShortcut]`r`nURL=" + $_.web_url), [System.Text.Encoding]::GetEncoding('iso-8859-1'))
-
 }
